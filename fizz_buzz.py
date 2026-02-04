ead97139b6715b1b321194ad253e94f18bfba59e
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-02-04 22:39:50 +0000

    Add code to play the game

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,12 +1,19 @@
-print("Hello World!")
-a = 10
-b = 5
-if a == b:
-    print("Yes")
-else:
-    print("No")
 
 # Number divisible by 3 = "Fizz"
 # Number divisible by 5 = "Buzz"
 # Number divisible by 3 and 5 = "FizzBuzz"
 # Otherwise say the original number 
+
+num = int(input("Enter a number: "))
+
+# Modulus operator divides a number and gives the remainder
+# As we are checking if numbers divide exactly by 3 and 5, we meed there to be no remainder
+# after the division i.e. the modulus needs to be zero
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print("FizzBuzz")
