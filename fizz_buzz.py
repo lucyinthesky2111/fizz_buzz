cf7a05509fe507ff9710e254070b62633d295cd9
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-02-04 22:05:57 +0000

    Add comments to explain the game

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,3 +5,8 @@ if a == b:
     print("Yes")
 else:
     print("No")
+
+# Number divisible by 3 = "Fizz"
+# Number divisible by 5 = "Buzz"
+# Number divisible by 3 and 5 = "FizzBuzz"
+# Otherwise say the original number 
